f1b82659ac6505015a10c114b066fc7a61841e4c
dwarjie <macoycoder@gmail.com>
2024-12-14 00:09:19 +0800

    - Added a content script in the manifest.json - Removed the separate build for code mirror bundle

--- src/loader.js
+++ src/loader.js
@@ -1,7 +1,9 @@
 import editorRender from "./lib/EditorRender?script&module";
 
-const scriptElem = document.createElement("script");
-scriptElem.type = "module";
-scriptElem.src = chrome.runtime.getURL(editorRender);
-
-document.body.appendChild(scriptElem);
+(function () {
+  const scriptElem = document.createElement("script");
+  scriptElem.type = "module";
+  scriptElem.src = chrome.runtime.getURL(editorRender);
+  (document.head || document.documentElement).appendChild(scriptElem);
+  console.log(`Script ${scriptElem}`);
+})();
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { crx } from "@crxjs/vite-plugin";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
 import toUtf8 from "./utils/toUtf8";
 
